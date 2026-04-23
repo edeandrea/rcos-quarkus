303702b83d5886d29c17a23cb6f32c26c25b1c96
iamchwill <iamchwill@gmail.com>
2026-04-23 06:27:15 -0400

    Python check for shell code commands

--- chenw22/Tests/process.py
+++ chenw22/Tests/process.py
@@ -1,4 +1,14 @@
-## Moves files into proper directories
-## Renames certain aspects to fit the process
-## Bash version possible?
+import os
+MKDOCS_DIR = "docs/docs"
+OUTPUT_DIR = "roq-site"
+CONTENT_DIR = os.path.join(OUTPUT_DIR, "content")
+PUBLIC_DIR = os.path.join(OUTPUT_DIR, "public")
 
+def ensure_dirs():
+    os.makedirs(CONTENT_DIR, exist_ok=True)
+    os.makedirs(PUBLIC_DIR, exist_ok=True)
+
+
+if __name__ == "__main__":
+    ensure_dirs()
+    print("check")
